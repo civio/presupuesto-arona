72f0a57c0452cf9139180e0746dc3db6c8beb87c
David Cabo <david.cabo@gmail.com>
2017-09-02 15:47:47 +0200

    Add helper script

---
+++ copy_csv.sh
@@ -0,0 +1,19 @@
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2013/gastos.csv data/es/municipio/2013/gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2013/gastos.csv data/es/municipio/2013/ejecucion_gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2013/ingresos.csv data/es/municipio/2013/ingresos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2013/ingresos.csv data/es/municipio/2013/ejecucion_ingresos.csv
+
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2014/gastos.csv data/es/municipio/2014/gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2014/gastos.csv data/es/municipio/2014/ejecucion_gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2014/ingresos.csv data/es/municipio/2014/ingresos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2014/ingresos.csv data/es/municipio/2014/ejecucion_ingresos.csv
+
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2015/gastos.csv data/es/municipio/2015/gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2015/gastos.csv data/es/municipio/2015/ejecucion_gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2015/ingresos.csv data/es/municipio/2015/ingresos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2015/ingresos.csv data/es/municipio/2015/ejecucion_ingresos.csv
+
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2016/gastos.csv data/es/municipio/2016/gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2016/gastos.csv data/es/municipio/2016/ejecucion_gastos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2016/ingresos.csv data/es/municipio/2016/ingresos.csv
+cp /Users/David/Box\ Sync/Civio/Proyectos/13\ Transparencia\ Municipal/40\ DVMI\ Arona\ \(Tenerife\)/02\ Datos/csv/2016/ingresos.csv data/es/municipio/2016/ejecucion_ingresos.csv
