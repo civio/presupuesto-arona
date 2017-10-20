98163232781f55a59e28fd69ebaa1690cbceb122
David Cabo <david.cabo@gmail.com>
2017-10-20 15:21:50 +0200

    Extend loading script

--- load_data.sh
+++ load_data.sh
@@ -5,3 +5,7 @@ python ../manage.py load_budget 2013-2016 municipio "Patronato Municipal de Turi
 python ../manage.py load_budget 2013-2016 municipio "Patronato Municipal de Servicios Sociales"
 
 python ../manage.py load_payments 2013-2016 municipio "Ayuntamiento de Arona"
+python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Cultura"
+python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Deportes"
+python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Turismo"
+python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Servicios Sociales"
