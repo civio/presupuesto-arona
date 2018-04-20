37577a5115dc13d049feab97c9338c59523225a5
David Cabo <david.cabo@gmail.com>
2018-04-20 21:24:42 +0200

    Refactor and clean up helper scripts In preparation for civio/presupuesto-management#699

--- load_data.sh
+++ load_data.sh
@@ -1,11 +1,14 @@
-python ../manage.py load_budget 2013-2016 municipio "Ayuntamiento de Arona"
-python ../manage.py load_budget 2013-2016 municipio "Patronato Municipal de Cultura"
-python ../manage.py load_budget 2013-2016 municipio "Patronato Municipal de Deportes"
-python ../manage.py load_budget 2013-2016 municipio "Patronato Municipal de Turismo"
-python ../manage.py load_budget 2013-2016 municipio "Patronato Municipal de Servicios Sociales"
+for YEAR in 2013 2014 2015 2016 2017
+do
+  python ../manage.py load_budget $YEAR municipio "Ayuntamiento de Arona"
+  python ../manage.py load_budget $YEAR municipio "Patronato Municipal de Cultura"
+  python ../manage.py load_budget $YEAR municipio "Patronato Municipal de Deportes"
+  python ../manage.py load_budget $YEAR municipio "Patronato Municipal de Turismo"
+  python ../manage.py load_budget $YEAR municipio "Patronato Municipal de Servicios Sociales"
 
-python ../manage.py load_payments 2013-2016 municipio "Ayuntamiento de Arona"
-python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Cultura"
-python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Deportes"
-python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Turismo"
-python ../manage.py load_payments 2013-2016 municipio "Patronato Municipal de Servicios Sociales"
+  python ../manage.py load_payments $YEAR municipio "Ayuntamiento de Arona"
+  python ../manage.py load_payments $YEAR municipio "Patronato Municipal de Cultura"
+  python ../manage.py load_payments $YEAR municipio "Patronato Municipal de Deportes"
+  python ../manage.py load_payments $YEAR municipio "Patronato Municipal de Turismo"
+  python ../manage.py load_payments $YEAR municipio "Patronato Municipal de Servicios Sociales"
+done
