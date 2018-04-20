c1c2b051a2291e45a662de171aad315f213bd5e7
David Cabo <david.cabo@gmail.com>
2018-04-20 21:25:32 +0200

    Add support for new payment field, ic_code. We are going to make the field optional in civio/presupuesto-management#694, but not done yet.

--- loaders/arona_payments_loader.py
+++ loaders/arona_payments_loader.py
@@ -8,8 +8,12 @@ class AronaPaymentsLoader(PaymentsLoader):
 
     # Parse an input line into fields
     def parse_item(self, budget, line):
+        # Skip blank lines
+        if line[0]=='':
+            return None
 
-        policy_id = line[1].strip()[:2] # First two digits of the programme make the policy id
+        # First two digits of the programme make the policy id
+        policy_id = line[1].strip()[:2]
         # But what we want as area is the policy description
         policy = Budget.objects.get_all_descriptions(budget.entity)['functional'][policy_id]
 
@@ -18,6 +22,7 @@ class AronaPaymentsLoader(PaymentsLoader):
             'programme': None,
             'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
             'ec_code': None,
+            'ic_code': None,
             'date': line[3].strip(),
             'payee': self._titlecase(line[5].strip()),
             'anonymized': False,
